184aed699263a0562c30c391e784187b87e2b1c6
rxt30 <mhampel@pm.me>
2022-02-01 17:22:34 +0100

    Added a general manager for database operations

---
+++ src/routes/Manager.ts
@@ -0,0 +1,27 @@
+import { getManager, EntityManager } from 'typeorm';
+
+export const saveObject = async (obj: object) => {
+  const manager: EntityManager = getManager();
+  await manager.save(obj);
+};
+
+export const getOneObject = async (
+  findOptions: object,
+  objectType: Function,
+): Promise<object> => {
+  const hasNull: boolean = Object.values(findOptions)
+    .every((x) => x === null || x === '' || x === undefined);
+  if (hasNull) {
+    throw new Error('Search object undefined');
+  }
+  const manager: EntityManager = getManager();
+  return manager.findOneOrFail(objectType, findOptions);
+};
+
+export const getObjects = async (
+  findOptions: object,
+  objectType: Function,
+): Promise<object[]> => {
+  const manager: EntityManager = getManager();
+  return manager.find(objectType, findOptions);
+};
