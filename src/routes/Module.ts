3c3f8edb19226a7906613a0e077ddeae4cc21dc0
DerNoob24 <74051636+DerNoob24@users.noreply.github.com>
2022-02-02 10:31:22 +0100

    added functions to module

--- src/routes/Module.ts
+++ src/routes/Module.ts
@@ -1,5 +1,6 @@
 import { Request, Respone } from 'express';
 import { getConnection, getRepository, Repository } from 'typeorm';
+import { deleteObjects, getOneObject, saveObject } from './Manager';
 import Module from '../entity/Module';
 import Course from '../entity/Course';
 import User from '../entity/User';
@@ -14,7 +15,32 @@ interface RegisterModule {
   assignedCourse: Course;
   submodule: Module[];
 }
+/**
+ * Representation of the incoming data of a module Request
+ * @interface
+ */
+interface GeneralModule {
+  id?: number;
+  name?: string;
+  description?: string;
+  assignedTeacher?: User[];
+  assignedCourse?: Course;
+  submodule?: Module[];
+  seniormodule?: Module;
+}
 
+/**
+ * Return the module with the given ID
+ * @param {GeneralModule} data - Data from the Requst
+ * @returns {Module}
+ */
+const getModule = (data: GeneralModule): Module => {
+  if (data.id == null) {
+    throw new Error();
+  }
+  const module: any = getOneObject({ where: { id: data.id } }, Module);
+  return module;
+};
 /**
  * Creates a new Module with the given data
  * @param {RegisterModule} data - Data of the new module
@@ -86,8 +112,14 @@ export const registerModule = (req: Request, res: Respone) => {
  * @param {Response} res- Used to form the response
  */
 export const deleteModule = async (req: Request, res: Respone) => {
-  const data = req.body;
-  const [module, moduleRepository] = await getModuleAndRepo(data.name);
+  try {
+    const data: GeneralModule = req.body;
+    const module: Module = getModule(data);
+    await deleteObjects(module, Module);
+    res.status(200).send('The Module has been deleted');
+  } catch (_err) {
+    res.status(500).send('Module could not be deleted');
+  }
 };
 
 /**
@@ -98,7 +130,17 @@ export const deleteModule = async (req: Request, res: Respone) => {
  * @param {Response} res- Used to form the response
  */
 export const addTeacher = async (req: Request, res: Respone) => {
-
+  try {
+    const data: GeneralModule = req.body;
+    const module: Module = getModule(data);
+    const teacher: User[] = module.assignedTeacher;
+    teacher.concat(data.assignedTeacher);
+    module.assignedTeacher = teacher;
+    await saveObject(module);
+    res.status(200).send('The Teachers have been added');
+  } catch (_err) {
+    res.status(500).send('Teachers could not be added');
+  }
 };
 
 /**
@@ -109,7 +151,22 @@ export const addTeacher = async (req: Request, res: Respone) => {
  * @param {Response} res- Used to form the response
  */
 export const deleteTeacher = async (req: Request, res: Respone) => {
-
+  try {
+    const data: GeneralModule = req.body;
+    const module: Module = getModule(data);
+    const teacher: User[] = module.assignedTeacher;
+    data.assignedTeacher.forEach((element) => {
+      const index = teacher.indexOf(element);
+      if (index > -1) {
+        teacher.splice(index, 1);
+      }
+    });
+    module.assignedTeacher = teacher;
+    await saveObject(module);
+    res.status(200).send('The Teachers have been removed');
+  } catch (_err) {
+    res.status(500).send('Teachers could not be removed');
+  }
 };
 
 /**
@@ -164,5 +221,4 @@ export const addSubmodule = async (req: Request, res: Respone) => {
  * @param {Response} res- Used to form the response
  */
 export const deleteSubmodule = async (req: Request, res: Respone) => {
-
 };
