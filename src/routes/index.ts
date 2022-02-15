186e756d5a94d0dc632beb126dbe1a21007161ba
DerNoob24 <74051636+DerNoob24@users.noreply.github.com>
2022-02-15 22:42:04 +0100

    updated imports

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,25 +1,12 @@
 import * as express from 'express';
 import * as jwt from 'jsonwebtoken';
-import {
-  registerUser, loginUser, deleteUser, changeUserPassword, getAllUsers,
-} from './User';
-import {
-  addCourse, addSubmodule, deleteSubmodule, addTeacher, deleteTeacher, changeDescription, changeName,
-  deleteModule, registerModule, deleteCourse as removeCourse, selectModule,
-} from './Module';
-import {
-  changeCourse, addStudent, deleteStudent, deleteCourse, registerCourse, selectCourse,
-} from './Course';
-import {
-  getGradesForStudent, insertGradeForStudent, deleteGradeForStudent, getGradesForModule,
-} from './Grades';
-import {
-  deleteTimeTableEntriesModule,
-  getTimeTableEntriesCourse, getTimeTableEntriesModule,
-  getTimeTableEntriesPerson, insertTimetableEntry,
-} from './TimeTable';
+import * as user from './User';
+import * as module from './Module';
+import * as course from './Course';
+import * as grades from './Grades';
+import * as time from './TimeTable';
 import * as Permission from './PermissionCheck';
-import { registerModuleItem, selectAllModuleItems, selectModuleItem } from './ModuleItem';
+import * as moduleItem from './ModuleItem';
 
 export interface JwtPayload {
   'id': number,
@@ -49,145 +36,145 @@ router.use((req: express.Request, res: express.Response, next: express.Next) => 
 
 // API Calls for User
 router.post('/user/register', (req: express.Request, res: express.Response) => {
-  Permission.checkAdministrator(req, res, registerUser);
+  Permission.checkAdministrator(req, res, user.registerUser);
 });
 
 router.post('/user/login', (req: express.Request, res: express.Response) => {
-  loginUser(req, res);
+  user.loginUser(req, res);
 });
 
 router.post('/user/delete', (req: express.Request, res: express.Response) => {
-  Permission.checkAdministrator(req, res, deleteUser);
+  Permission.checkAdministrator(req, res, user.deleteUser);
 });
 
 router.post('/user/changePassword', (req: express.Request, res: express.Response) => {
-  Permission.checkAdministratorOrOwnUsername(req, res, changeUserPassword);
+  Permission.checkAdministratorOrOwnUsername(req, res, user.changeUserPassword);
 });
 
 router.get('/user/getAll', (req: express.Request, res: express.Response) => {
-  Permission.checkAdministrator(req, res, getAllUsers);
+  Permission.checkAdministrator(req, res, user.getAllUsers);
 });
 
 // API Calls for Module
 router.post('/module/register', (req: express.Request, res: express.Response) => {
-  Permission.checkAdministrator(req, res, registerModule);
+  Permission.checkAdministrator(req, res, module.registerModule);
 });
 
 router.post('/module/:moduleId/changeName', (req: express.Request, res: express.Respone) => {
-  Permission.checkAdministrator(req, res, changeName);
+  Permission.checkAdministrator(req, res, module.changeName);
 });
 
 router.post('/module/:moduleId/addSubmodule', (req: express.Request, res: express.Respone) => {
-  Permission.checkAdministrator(req, res, addSubmodule);
+  Permission.checkAdministrator(req, res, module.addSubmodule);
 });
 
 router.post('/module/:moduleId/deleteSubmodule', (req: express.Request, res: express.Respone) => {
-  Permission.checkAdministrator(req, res, deleteSubmodule);
+  Permission.checkAdministrator(req, res, module.deleteSubmodule);
 });
 
 router.post('/module/:moduleId/changeDescription', (req: express.Request, res: express.Respone) => {
-  Permission.checkAdministrator(req, res, changeDescription);
+  Permission.checkAdministrator(req, res, module.changeDescription);
 });
 
 router.post('/module/:moduleId/deleteCourse', (req: express.Request, res: express.Respone) => {
-  removeCourse(req, res);
+  module.deleteCourse(req, res);
 });
 
 router.post('/module/:moduleId/addCourse', (req: express.Request, res: express.Respone) => {
-  Permission.checkAdministrator(req, res, addCourse);
+  Permission.checkAdministrator(req, res, module.addCourse);
 });
 
 router.post('/module/:moduleId/addTeacher', (req: express.Request, res: express.Respone) => {
-  Permission.checkAdministrator(req, res, addTeacher);
+  Permission.checkAdministrator(req, res, module.addTeacher);
 });
 
 router.post('/module/:moduleId/deleteTeacher', (req: express.Request, res: express.Respone) => {
-  Permission.checkAdministrator(req, res, deleteTeacher);
+  Permission.checkAdministrator(req, res, module.deleteTeacher);
 });
 
 router.post('/module/:moduleId/deleteModule', (req: express.Request, res: express.Respone) => {
-  Permission.checkAdministrator(req, res, deleteModule);
+  Permission.checkAdministrator(req, res, module.deleteModule);
 });
 
 router.get('/module/:moduleId', (req: express.Request, res: express.Respone) => {
-  selectModule(req, res);
+  module.selectModule(req, res);
 });
 
 // API Calls for ModuleItem
 router.post('/module/:moduleId/addMouduleItem', (req: express.Request, res: express.Respone) => {
-  registerModuleItem(req, res);
+  moduleItem.registerModuleItem(req, res);
 });
 
 router.get('/module/:moduleId/:moduleItemId', (req: express.Request, res: express.Respone) => {
-  selectModuleItem(req, res);
+  moduleItem.selectModuleItem(req, res);
 });
 
 router.get('/module/:moduleId/selectModuleItems', (req: express.Request, res: express.Respone) => {
-  selectAllModuleItems(req, res);
+  moduleItem.selectAllModuleItems(req, res);
 });
 
 // API Calls for Course
 router.get('/course/:courseId', (req: express.Request, res: express.Response) => {
-  selectCourse(req, res);
+  course.selectCourse(req, res);
 });
 
 router.post('/course/:courseId/changeCourse', (req: express.Request, res: express.Response) => {
-  Permission.checkAdministrator(req, res, changeCourse);
+  Permission.checkAdministrator(req, res, course.changeCourse);
 });
 
 router.post('/course/register', (req: express.Request, res: express.Response) => {
-  Permission.checkAdministrator(req, res, registerCourse);
+  Permission.checkAdministrator(req, res, course.registerCourse);
 });
 
 router.post('/course/:courseId/delete', (req: express.Request, res: express.Response) => {
-  Permission.checkAdministrator(req, res, deleteCourse);
+  Permission.checkAdministrator(req, res, course.deleteCourse);
 });
 
 router.post('/course/:courseId/addStudent', (req: express.Request, res: express.Response) => {
-  Permission.checkAdministrator(req, res, addStudent);
+  Permission.checkAdministrator(req, res, course.addStudent);
 });
 
 router.post('/course/:courseId/deleteStudent', (req: express.Request, res: express.Response) => {
-  Permission.checkAdministrator(req, res, deleteStudent);
+  Permission.checkAdministrator(req, res, course.deleteStudent);
 });
 
 // API Calls for Grades
 router.get('/grades/:studentId', (req: express.Request, res: express.Response) => {
   req.body.id = parseInt(req.params.studentId, 10);
-  Permission.checkAdministratorOrOwnID(req, res, getGradesForStudent);
+  Permission.checkAdministratorOrOwnID(req, res, grades.getGradesForStudent);
 });
 
 router.get('/grades/module/:moduleId', (req: express.Request, res: express.Response) => {
-  Permission.checkAdministratorOrModuleTeacher(req, res, getGradesForModule);
+  Permission.checkAdministratorOrModuleTeacher(req, res, grades.getGradesForModule);
 });
 
 router.post('/grades/insert', (req: express.Request, res: express.Response) => {
-  Permission.checkAdministratorOrModuleTeacher(req, res, insertGradeForStudent);
+  Permission.checkAdministratorOrModuleTeacher(req, res, grades.insertGradeForStudent);
 });
 
 router.post('/grades/delete', (req: express.Request, res: express.Response) => {
-  Permission.checkAdministratorOrModuleTeacher(req, res, deleteGradeForStudent);
+  Permission.checkAdministratorOrModuleTeacher(req, res, grades.deleteGradeForStudent);
 });
 
 // API Calls for Timetable
 router.post('/timetable/insert', (req: express.Request, res: express.Response) => {
-  Permission.checkAdministratorOrModuleTeacher(req, res, insertTimetableEntry);
+  Permission.checkAdministratorOrModuleTeacher(req, res, time.insertTimetableEntry);
 });
 
 router.get('/timetable/getPerson', (req: express.Request, res: express.Response) => {
-  getTimeTableEntriesPerson(req, res);
+  time.getTimeTableEntriesPerson(req, res);
 });
 
 router.get('/timetable/getModule/:moduleId', (req: express.Request, res: express.Response) => {
-  getTimeTableEntriesModule(req, res);
+  time.getTimeTableEntriesModule(req, res);
 });
 
 router.get('/timetable/getCourse/:courseId', (req: express.Request, res: express.Response) => {
-  getTimeTableEntriesCourse(req, res);
+  time.getTimeTableEntriesCourse(req, res);
 });
 
 router.post('/timetable/delete', (req: express.Request, res: express.Response) => {
-  Permission.checkAdministratorOrModuleTeacher(req, res, deleteTimeTableEntriesModule);
+  Permission.checkAdministratorOrModuleTeacher(req, res, time.deleteTimeTableEntriesModule);
 });
 
 export default router;
